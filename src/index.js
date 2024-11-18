342c1e44a70c6dd0c6b0ff526df6949da37abb86
David Tso <davidtso1219@gmail.com>
2024-11-17 21:20:37 -0800

    Added sample source files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Standardizes a string by converting it to lowercase.
+ *
+ * @param {string} input - The string to be standardized.
+ * @returns {string} The standardized string, converted to lowercase.
+ */
+export function standardize_string(input) {
+  return input.toLowerCase().trim();
+}
