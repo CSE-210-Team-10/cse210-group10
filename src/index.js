2b38640c0439307b32cb5a2b6b843d03b9c161ec
David Tso <davidtso1219@gmail.com>
2024-11-21 22:19:27 -0800

    Fixed linting error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@
  * @param {string} input - The string to be standardized.
  * @returns {string} The standardized string, converted to lowercase.
  */
-export function standardize_string(input) {
+export function standardizeString(input) {
   return input.toLowerCase().trim();
 }
