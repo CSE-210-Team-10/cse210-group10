0fc52c69f1e5b6a6ccbbcc4dd8d5a16669aabc58
sammedkamate <sammedkamate007@gmail.com>
2024-12-06 13:16:54 -0800

    chatbot user query

--- src/chatbot.js
+++ src/chatbot.js
@@ -7,25 +7,35 @@ const client = new Groq({
     apiKey: process.env.GROQ_API_KEY
 });
 
-async function chat() {
-    const chatCompletion = await client.chat.completions.create({
-        messages: [
-            {
-                role: 'system',
-                content: 'You are a helpful assistant.'
-            },
-            {
-                role: 'user',
-                content: 'Hello, how are you?'
-            }
-        ],
-        model: 'gemma-7b-it'
-    });
 
-    console.log(chatCompletion.choices[0].message.content);
-    return chatCompletion.choices[0].message.content;
+async function chat(userQuery) {
+
+    try{
+        const chatCompletion = await client.chat.completions.create({
+            messages: [
+                {
+                    role: 'system',
+                    content: `You are a helpful Student Assistant.
+                                Guidelines:
+                                1. Please respond strictly within 100 words. 
+                                2. Do not make information up.
+                                3. Be consise with your response.
+                                4. Only provide specifc answer. Do not provide other information.`
+                },
+                {
+                    role: 'user',
+                    content: userQuery
+                }
+            ],
+            model: 'gemma-7b-it',
+            temperature: 0.7,
+            max_tokens: 80
+        });
+
+        const assistantResponse = chatCompletion.choices[0].message.content;
+        return assistantResponse;
+    }catch(error){
+        console.error('Error:', error);
+        return 'Could not fetch the information required. Please try again!';
+    }
 }
-
-chat().then( output => {
-    console.log(output);
-  });
