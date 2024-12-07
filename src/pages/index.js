8aae610728df2ebfea4af94243de6f41c46725f5
Matthew Chan <72158949+blankss@users.noreply.github.com>
2024-12-06 22:33:31 -0800

    Added getIssues and getPullRequests

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,16 @@ function redirectToLogin() {
  */
 function renderPage(user) {
   console.log(user);
+
+  const token = user.accessToken;
+
+  console.log(token); 
+
+  const dataIssues = await getIssues(token, owner, repo);
+  const dataPulls = await getPullRequests(token, owner, repo);
+  
+  console.log(dataIssues);
+  console.log(dataPulls);
 }
 
 /**
@@ -33,5 +43,67 @@ function authEventHandler(event, user) {
     renderPage(authService.getGithubData());
   } else if (event === 'SIGNED_OUT' || !user) {
     redirectToLogin();
+  }
+}
+
+//Constant variables just for proof of concept, remove when deploying/releasing 
+const owner = 'CSE-210-Team-10';
+const repo = 'group10-esc012';
+
+/**
+ * Fetch pull request data from specified parameters.
+ * @param { string } token The SSO token generated
+ * @param { string } owner The owner of the repo
+ * @param { string } repo The repo that the user wants to pull from
+ */
+async function getPullRequests(token, owner, repo) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/pulls`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `${token}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch pull requests.');
+    }
+
+    const data = await response.json();
+    return data;
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Fetch issue data from specified parameters.
+ * @param { string } token The SSO token generated
+ * @param { string } owner The owner of the repo
+ * @param { string } repo The repo that the user wants to pull from
+ */
+async function getIssues(token, owner, repo) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `${token}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch issues.');
+    }
+
+    const data = await response.json();
+    return data;
+
+  }
+  catch (error) {
+    console.log(error);
   }
 }
