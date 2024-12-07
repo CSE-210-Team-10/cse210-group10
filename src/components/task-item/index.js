0b3749e7fa4616ad384966cad1c8634cea7ead3d
David Tso <davidtso1219@gmail.com>
2024-12-07 01:16:58 -0800

    Added JS Docs

--- src/components/task-item/index.js
+++ src/components/task-item/index.js
@@ -4,23 +4,55 @@ import html from './template.html';
 const sheet = new CSSStyleSheet();
 sheet.replaceSync(css);
 
+const DataAttributeSelector = {
+  title: 'data-title',
+  priority: 'data-priority',
+  date: 'data-date',
+  tags: 'data-tags',
+};
+
+const UISelector = {
+  title: '.task-title',
+  priority: '.task-priority',
+  date: '.task-date',
+  tags: '.task-tags',
+  editBtn: '.edit-button',
+  deleteBtn: '.delete-button',
+  completeBtn: '.complete-button',
+};
+
+/**
+ * A custom component that displays details of a task and provides interaction
+ */
 export class TaskItem extends HTMLElement {
+  /**
+   *
+   */
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.adoptedStyleSheets = [sheet];
   }
 
+  /**
+   * @returns { string[] } A list of attributes to listen to
+   */
   static get observedAttributes() {
-    return ['data-title', 'data-priority', 'data-date', 'data-tags'];
+    return Object.values(DataAttributeSelector); //
   }
 
+  /**
+   * Render and add event listeners when the component is mounted to the DOM
+   */
   connectedCallback() {
     this.mountTemplate();
     this.render();
     this.addEventListeners();
   }
 
+  /**
+   * Render the page whenever an attribute is updated
+   */
   attributeChangedCallback() {
     // Only update content if template is already mounted
     if (this.shadowRoot.querySelector('details')) {
@@ -28,26 +60,64 @@ export class TaskItem extends HTMLElement {
     }
   }
 
+  /**
+   * @returns { string } value in data-title attribute
+   */
+  get title() {
+    return this.getAttribute(DataAttributeSelector.title) || '';
+  }
+
+  /**
+   * @returns { string } value in data-priority attribute
+   */
+  get priority() {
+    return this.getAttribute(DataAttributeSelector.priority) || '';
+  }
+
+  /**
+   * @returns { string } value in data-date attribute
+   */
+  get date() {
+    return this.getAttribute(DataAttributeSelector.date) || '';
+  }
+
+  /**
+   * @returns { string[] } a parsed string array of data-tags attribute
+   * @throws { Error } if data-tags is non parseable
+   */
+  get tags() {
+    try {
+      return JSON.parse(this.getAttribute(DataAttributeSelector.tags) || '[]');
+    } catch (error) {
+      console.error('Error parsing tags:', error);
+      return [];
+    }
+  }
+
+  /**
+   * Mount HTML code to the component
+   */
   mountTemplate() {
     this.shadowRoot.innerHTML = html;
   }
 
+  /**
+   * Render the component
+   */
   render() {
     // Get all attributes
-    const title = this.getAttribute('data-title') || '';
-    const priority = this.getAttribute('data-priority') || '';
-    const date = this.getAttribute('data-date') || '';
+    const { title, priority, date, tags } = this;
 
     // Update title
-    const titleElement = this.shadowRoot.querySelector('.task-title');
+    const titleElement = this.shadowRoot.querySelector(UISelector.title);
     if (titleElement) titleElement.textContent = title;
 
     // Update date
-    const dateElement = this.shadowRoot.querySelector('.task-date');
+    const dateElement = this.shadowRoot.querySelector(UISelector.date);
     if (dateElement) dateElement.textContent = date;
 
     // Update tags
-    const tagsContainer = this.shadowRoot.querySelector('.tags');
+    const tagsContainer = this.shadowRoot.querySelector(UISelector.tags);
     if (!tagsContainer) return;
 
     tagsContainer.innerHTML = '';
@@ -61,26 +131,26 @@ export class TaskItem extends HTMLElement {
     }
 
     // Handle additional tags from JSON
-    try {
-      /** @type {string[]} */
-      const tags = JSON.parse(this.getAttribute('data-tags') || '[]');
-
-      tags.forEach(tag => {
-        const tagElement = document.createElement('li');
-        tagElement.className = 'tag';
-        tagElement.textContent = tag;
-        tagsContainer.appendChild(tagElement);
-        console.log(tag);
-      });
-    } catch (e) {
-      console.error('Failed to parse tags:', e);
-    }
+    tags.forEach(tag => {
+      const tagElement = document.createElement('li');
+      tagElement.className = 'tag';
+      tagElement.textContent = tag;
+      tagsContainer.appendChild(tagElement);
+      console.log(tag);
+    });
   }
 
+  /**
+   * Add event listenrs to buttons and slots
+   * Fire custom events for button clicked
+   * And check description length for slot changed
+   */
   addEventListeners() {
-    const editButton = this.shadowRoot.querySelector('.edit-button');
-    const deleteButton = this.shadowRoot.querySelector('.delete-button');
-    const completeButton = this.shadowRoot.querySelector('.complete-button');
+    const editButton = this.shadowRoot.querySelector(UISelector.editBtn);
+    const deleteButton = this.shadowRoot.querySelector(UISelector.deleteBtn);
+    const completeButton = this.shadowRoot.querySelector(
+      UISelector.completeBtn
+    );
 
     editButton?.addEventListener('click', e => {
       e.preventDefault();
