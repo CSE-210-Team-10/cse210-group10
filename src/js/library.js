efe25f836cc67e58e1c25d87fefa4978a4becae0
Yuxuan-Wu <yuw095@ucsd.edu>
2024-12-06 17:03:41 -0800

    Task filtering methods are created, now tasks can be filtered on type, status, creation time

--- src/js/library.js
+++ src/js/library.js
@@ -7,15 +7,3 @@
 export function standardizeString(input) {
   return input.toLowerCase().trim();
 }
-
-/**
- * Filter an array of JSON objects based on a key-value pair.
- *
- * @param {Array} array - The array of JSON objects to be filtered.
- * @param {string} key - The key to filter by.
- * @param {string} value - The value to filter by.
- * @returns {Array} The filtered array of JSON objects.
- */
-export function filterByProperty(array, key, value) {
-  return array.filter((item) => item[key] === value);
-}
