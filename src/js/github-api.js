bf3d5b3be10ef2ad3f03e02f9ed6f10883ba3ca8
Matthew Chan <hc002@ucsd.edu>
2024-12-07 17:23:24 -0800

    Moved respective functions into github-api.js from index.js in pages

--- src/js/github-api.js
+++ src/js/github-api.js
@@ -1,3 +1,5 @@
+import { authService } from './auth.js';
+
 /**
  * Verify if the GitHub provider token is still valid
  * @param { string } token github provider token
@@ -18,5 +20,65 @@ export async function isProviderTokenValid(token) {
   } catch (error) {
     console.error('Error checking token validity:', error);
     return false;
+  }
+}
+
+/**
+ * Fetch pull request data from specified parameters.
+ * @param { string } token The SSO token generated
+ * @param { string } owner The owner of the repo
+ * @param { string } repo The repo that the user wants to pull from
+ * @returns { string } returns the pull requests data in json format
+ */
+export async function getPullRequests(token, owner, repo) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/pulls`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `${token}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch pull requests.');
+    }
+
+    const data = await response.json();
+    return data;
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Fetch issue data from specified parameters.
+ * @param { string } token The SSO token generated
+ * @param { string } owner The owner of the repo
+ * @param { string } repo The repo that the user wants to pull from
+ * @returns { string } returns the issues data in json format
+ */
+export async function getIssues(token, owner, repo) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `${token}`,
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch issues.');
+    }
+
+    const data = await response.json();
+    return data;
+
+  }
+  catch (error) {
+    console.log(error);
   }
 }
