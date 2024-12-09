3fab98f630d19c5412fc75568020d707449c7db6
David Tso <davidtso1219@gmail.com>
2024-12-09 03:30:06 -0800

    Implemented A Few Functionalities
    1. Render Personal Tasks with TaskStore
    2. Create task

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import { authService } from '../js/auth.js';
-import { standardizeString } from '../js/library.js';
 import { TaskItem } from '../components/task-item/index.js';
 import { TaskForm } from '../components/task-form/index.js';
+import TaskStore from '../js/task/crud.js';
 
+/** @typedef { import('../js/task/index.js').Task } Task */
 /** @typedef { import('../js/auth.js').UserData } User */
 
-console.log(standardizeString('test'));
+/**
+ * @typedef {object} TaskFormData
+ * @property {string} taskName - Name/title of the task
+ * @property {'high' | 'medium' | 'low'} priority - Priority level of the task
+ * @property {string[]} tags - Array of tags associated with the task
+ * @property {string} dueDate - Due date string from the form
+ * @property {string} description - Description of the task
+ */
+
+/** @type { 'create' | 'edit' | null } current state of the task form */
+let taskFormMode = null;
 
 /** @type { TaskForm } */
 const taskForm = document.querySelector('task-form');
@@ -30,6 +41,7 @@ function redirectToLogin() {
  */
 async function renderPage(user) {
   console.log(user);
+  renderTaskPanels(TaskStore.getAllTasks());
 }
 
 /**
@@ -53,15 +65,53 @@ function authEventHandler(event, user) {
 function openTaskForm() {
   /** @type { TaskForm } */
   const taskForm = document.querySelector('task-form');
+  taskFormMode = 'create';
   taskForm.show();
 }
 
 /**
- * Handle the fired event when the user creates a new task
+ * Convert form data to task format
+ * @param { TaskFormData } formData The data from the form submission event
+ * @returns { Omit<Task, 'id'> } Formatted task data
+ */
+function formatTaskData(formData) {
+  return {
+    title: formData.taskName,
+    type: 'personal',
+    done: false,
+    priority: formData.priority,
+    tags: formData.tags,
+    dueDate: new Date(formData.dueDate),
+    description: formData.description,
+    url: '',
+  };
+}
+
+/**
+ * Handle the fired event when the user creates or edits a task
  * @param { CustomEvent } e The custom event object passed from task-form
  */
 function handleTaskFormSubmit(e) {
-  console.log(e.detail);
+  if (!taskFormMode)
+    throw new Error(
+      'Task form mode should not be null when the task form is submitted.'
+    );
+
+  const taskData = formatTaskData(e.detail);
+
+  try {
+    if (taskFormMode === 'create') {
+      TaskStore.createTask(taskData);
+      renderTaskPanels(TaskStore.getAllTasks());
+    } else if (taskFormMode === 'edit') {
+      // if (!currentTaskId) {
+      //   throw new Error('Task ID is required for edit mode');
+      // }
+      // TaskStore.updateTask(currentTaskId, formattedData);
+    }
+  } catch (error) {
+    console.error('Failed to process task:', error);
+  }
 }
 
 /**
@@ -78,6 +128,7 @@ function handleTaskEdit(e) {
     dueDate: e.detail.date,
     description: e.detail.description,
   });
+  taskFormMode = 'edit';
   taskForm.show();
 }
 
@@ -95,4 +146,55 @@ function handleTaskDelete(e) {
  */
 function handleTaskCompleted(e) {
   console.log(e.detail);
+}
+
+/**
+ * Creates a task-item element with the given task data
+ * @param { Task } task - The task data to create an element for
+ * @param { boolean } interactive - Whether this task is interactive
+ * @returns { HTMLElement } The created task-item element
+ */
+function createTaskElement(task, interactive) {
+  const li = document.createElement('li');
+  const taskItem = document.createElement('task-item');
+
+  // Set the data attributes
+  taskItem.dataset.id = String(task.id);
+  taskItem.dataset.title = task.title;
+  taskItem.dataset.priority = task.priority;
+  taskItem.dataset.tags = JSON.stringify(task.tags);
+  taskItem.dataset.date = task.dueDate.toLocaleDateString();
+  if (interactive) taskItem.setAttribute('interactive', '');
+
+  // Set the description as the content
+  taskItem.textContent = task.description;
+
+  li.appendChild(taskItem);
+  return li;
+}
+
+/**
+ * Populate tasks on view based on the type of the task
+ * If a task is type "issue" or "PR", then populate it on GitHub Tasks
+ * If a task is type "personal" then populate it on Personal Tasks
+ * @param { Task[] } tasks a list of tasks to populate
+ */
+function renderTaskPanels(tasks) {
+  const personalTasksList = document.querySelector(
+    '#personal-task-panel .tasks'
+  );
+
+  personalTasksList.innerHTML = '';
+
+  // Sort tasks into appropriate lists
+  tasks.forEach(task => {
+    const taskElement = createTaskElement(task, true);
+
+    // Determine which list to add the task to
+    if (task.type === 'personal') {
+      personalTasksList.appendChild(taskElement);
+    } else if (task.type === 'issue' || task.type === 'pr') {
+      // githubTasksList.appendChild(taskElement);
+    }
+  });
 }
