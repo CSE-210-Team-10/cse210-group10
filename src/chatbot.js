63fd9abd641f1b0eeb276486aee9184511c7b51a
sammedkamate <sammedkamate007@gmail.com>
2024-12-08 19:18:21 -0800

    seperated event handler

--- src/chatbot.js
+++ src/chatbot.js
@@ -146,22 +146,7 @@ export async function chat(userMessage) {
     }
 };
 
-//event listener - button click
-document.getElementById('submit-button').addEventListener('click', async () => {
-    const userInput = document.getElementById('input-box').value;
-    
-    if (userInput) {
-        const chatBox = document.getElementById('chat-box');
-        chatBox.innerHTML += `<div><strong>You:</strong> ${userInput}</div>`;
-        
-        const response = await chat(userInput);  
 
-        chatBox.innerHTML += `<div><strong>Bot:</strong> ${response}</div>`;
-        document.getElementById('input-box').value = '';  
-        
-        chatBox.scrollTop = chatBox.scrollHeight; 
-    }
-});
   
 
 // Example 
