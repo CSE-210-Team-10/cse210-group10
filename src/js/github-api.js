3a390f804a9c3b66d700160bdc74cb4ca54c0c52
Matthew Chan <hc002@ucsd.edu>
2024-12-11 00:42:14 -0800

    Added filter to filter incoming issues and prs by the logged in user.

--- src/js/github-api.js
+++ src/js/github-api.js
@@ -1,3 +1,4 @@
+/** @typedef { import('../js/auth.js').UserData } User */
 /**
  * Verify if the GitHub provider token is still valid
  * @param { string } token github provider token
@@ -23,18 +24,18 @@ export async function isProviderTokenValid(token) {
 
 /**
  * Fetch pull request data from specified parameters.
- * @param { string } token The SSO token generated
+ * @param { User } user The SSO token generated
  * @param { string } owner The owner of the repo
  * @param { string } repo The repo that the user wants to pull from
  * @returns { Promise<object[]> } returns the pull requests data task object format
  */
-export async function getPullRequests(token, owner, repo) {
+export async function getPullRequests(user, owner, repo) {
   const url = `https://api.github.com/repos/${owner}/${repo}/pulls`;
 
   try {
     const response = await fetch(url, {
       headers: {
-        'Authorization': `${token}`,
+        'Authorization': `${user.accessToken}`,
         'X-GitHub-Api-Version': '2022-11-28'
       }
     });
@@ -48,23 +49,29 @@ export async function getPullRequests(token, owner, repo) {
 
     //Create task object from fetched data and populate array to return - return pull requests
     for (let i = 0; i < data.length; i ++) {
-      const parsedTask = {
-        type: 'pr',
-        title: String(data[i].title),
-        done: false,
-        dueDate: data[i].created_at instanceof Date 
-          ? data[i].created_at 
-          : new Date(data[i].created_at ),
-        description: String(''),
-        url: String(data[i].html_url),
-        priority: String('high'),
-        tags: [owner, repo]
-      };
-      arrayPulls.push(parsedTask);
+      //Get the list of assignees and then parse them into a list of login usernames 
+      const tmp = data[i].assignees;
+      const assignees = []
+      for (let j = 0; j < tmp.length; j ++) {
+        assignees.push(tmp[j].login);
+      }
+      if (assignees.includes(user.accessToken)) {
+        const parsedTask = {
+          type: 'pr',
+          title: String(data[i].title),
+          done: false,
+          dueDate: data[i].created_at instanceof Date 
+            ? data[i].created_at 
+            : new Date(data[i].created_at ),
+          description: String(''),
+          url: String(data[i].html_url),
+          priority: String('high'),
+          tags: [owner, repo]
+        };
+        arrayPulls.push(parsedTask);
+      }
     }
-
     return arrayPulls;
-
   }
   catch (error) {
     console.error(error);
@@ -73,18 +80,18 @@ export async function getPullRequests(token, owner, repo) {
 
 /**
  * Fetch issue data from specified parameters.
- * @param { string } token The SSO token generated
+ * @param { User } user The SSO token generated
  * @param { string } owner The owner of the repo
  * @param { string } repo The repo that the user wants to pull from
  * @returns { Promise<object[]> } returns the issues in task object format 
  */
-export async function getIssues(token, owner, repo) {
+export async function getIssues(user, owner, repo) {
   const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
 
   try {
     const response = await fetch(url, {
       headers: {
-        'Authorization': `${token}`,
+        'Authorization': `${user.accessToken}`,
         'X-GitHub-Api-Version': '2022-11-28'
       }
     });
@@ -98,23 +105,29 @@ export async function getIssues(token, owner, repo) {
 
     //Create task object from fetched data and populate array to return - return issues
     for (let i = 0; i < data.length; i ++) {
-      const parsedIssue = {
-        type: 'issue',
-        title: String(data[i].title),
-        done: false,
-        dueDate: data[i].updated_at instanceof Date 
-          ? data[i].updated_at 
-          : new Date(data[i].updated_at ),
-        description: String(''),
-        url: String(data[i].url),
-        priority: String('high'),
-        tags: [owner, repo]
-      };
-      arrayIssues.push(parsedIssue);
+      //Get the list of assignees and then parse them into a list of login usernames 
+      const tmp = data[i].assignees;
+      const assignees = []
+      for (let j = 0; j < tmp.length; j ++) {
+        assignees.push(tmp[j].login);
+      }
+      if (assignees.includes(user.username)) {
+        const parsedIssue = {
+          type: 'issue',
+          title: String(data[i].title),
+          done: false,
+          dueDate: data[i].updated_at instanceof Date 
+            ? data[i].updated_at 
+            : new Date(data[i].updated_at ),
+          description: String(''),
+          url: String(data[i].url),
+          priority: String('high'),
+          tags: [owner, repo]
+        };
+        arrayIssues.push(parsedIssue);
+      }
     }
-
     return arrayIssues;
-
   }
   catch (error) {
     console.error(error);
